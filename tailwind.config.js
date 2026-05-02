956b3bf10d861f8f15dd12f68349bb437d67600f
Celia <hello@celiarozalenm.com>
2026-05-03 01:11:37 +0800

    Switch typography from Urbanist to Outfit

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['"Urbanist"', 'system-ui', '-apple-system', 'sans-serif'],
+        sans: ['"Outfit"', 'system-ui', '-apple-system', 'sans-serif'],
       },
     },
   },
